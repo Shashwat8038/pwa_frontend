82ec6804de869ce438002fc76b951a18365c5bb5
shashwat <shashwattiwari609@gmail.com>
2024-11-02 10:10:46 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,7 @@ function App() {
     const listenerRef = useRef(false); 
 
  
-    useEffect(() => {
-        // Register the Firebase Cloud Messaging service worker
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/firebase-messaging-sw.js')
-                .then((registration) => {
-                    console.log('Service Worker registered with scope:', registration.scope);
-                })
-                .catch((error) => {
-                    console.error('Service Worker registration failed:', error);
-                });
-        }
-    }, []);
+   
 
 
     useEffect(() => {
